cbab7c636bd4609f0297a046c4eacc91375d0f65
Nguyễn Minh Châu <91469379+chau-nm@users.noreply.github.com>
2025-01-04 19:09:35 +0700

    Time: 19 ms (54.1%), Space: 57.8 MB (50.92%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length;i++) {
+            if (i > k) {
+                set.remove(nums[i - k - 1]);
+            }
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+        }
+        return false;
+    }
+}
