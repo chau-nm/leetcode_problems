0a2f9cfca8257c7e759963829c408c60df96f65e
Nguyễn Minh Châu <91469379+chau-nm@users.noreply.github.com>
2025-01-04 18:37:18 +0700

    Time: 1 ms (99.82%), Space: 53.6 MB (6.31%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int candidate = nums[0];
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (count == 0) {
+                candidate = nums[i];
+                count = 1;
+            } else if (candidate == nums[i]) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return candidate;
+    }
+}
