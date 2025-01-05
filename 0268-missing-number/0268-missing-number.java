128ba77226ee98ee1819e0dd6c170fe131122162
Nguyễn Minh Châu <91469379+chau-nm@users.noreply.github.com>
2025-01-05 21:23:37 +0700

    Time: 0 ms (100%), Space: 45.2 MB (75.61%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,14 +1,9 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        Arrays.sort(nums);
-        if (nums[0] != 0) {
-            return 0;
+        int result = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            result += i - nums[i];
         }
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] - nums[i - 1] > 1) {
-                return nums[i] - 1;
-            }
-        }
-        return nums.length;
+        return result;
     }
 }
