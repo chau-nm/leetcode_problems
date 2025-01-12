07248408beb465f2a40abeaf4b06be304f428210
Nguyễn Minh Châu <91469379+chau-nm@users.noreply.github.com>
2025-01-12 14:38:45 +0700

    Time: 1 ms (81.26%), Space: 41.2 MB (96.28%) - LeetHub

---
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minimumMoves(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'X') {
+                count++;
+                i+=2;
+            }
+        }
+        return count;
+    }
+}
