c45c330705f27df7f05f451913a1272e9d23559d
Nguyễn Minh Châu <91469379+chau-nm@users.noreply.github.com>
2025-10-01 09:29:28 +0700

    Time: 5 ms (47.51%), Space: 4.7 MB (74.97%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.go
+++ 0242-valid-anagram/0242-valid-anagram.go
@@ -2,23 +2,23 @@ func isAnagram(s string, t string) bool {
     charMap := make(map[rune]int)
 
     for _, char := range s {
-        if val, found := charMap[char]; found {
-            charMap[char] = val + 1
+        if _, found := charMap[char]; found {
+            charMap[char]++
         } else {
             charMap[char] = 1
         }
     }
 
     for _, char := range t {
-        if val, found := charMap[char]; found {
-            charMap[char] = val - 1
+        if _, found := charMap[char]; found {
+            charMap[char]--
         } else {
             return false
         }
     }
 
     for _, val := range charMap {
-        if (val != 0) {
+        if val != 0 {
             return false
         }
     }
