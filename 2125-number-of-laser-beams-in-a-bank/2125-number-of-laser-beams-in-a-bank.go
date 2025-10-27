50b271e45e4e0cc62c339dac79ddc2fd12617857
Nguyễn Minh Châu <91469379+chau-nm@users.noreply.github.com>
2025-10-27 09:23:13 +0700

    Time: 0 ms (100%), Space: 9 MB (38.64%) - LeetHub

---
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.go
@@ -0,0 +1,19 @@
+func numberOfBeams(bank []string) int {
+    count:=0
+    preLaserNumber := 0
+    for i:=0; i < len(bank); i++ {
+        currentLaserNumber:=0
+        for _, ch := range bank[i] {
+            if ch == '1' {
+                currentLaserNumber++
+            }
+        }
+        if preLaserNumber > 0 && currentLaserNumber > 0 {
+            count += preLaserNumber * currentLaserNumber 
+        }
+        if currentLaserNumber > 0 {
+            preLaserNumber = currentLaserNumber
+        }
+    }
+    return count
+}
