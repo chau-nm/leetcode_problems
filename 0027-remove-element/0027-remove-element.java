d8465531d1e032775f68631a1b24ea6bdc9ea2ba
Nguyễn Minh Châu <91469379+chau-nm@users.noreply.github.com>
2026-04-09 11:10:14 +0700

    Time: 0 ms (100%), Space: 43.4 MB (68.86%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int slow = 0;
+        for (int fast = 0; fast < nums.length; fast++) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+        return slow;
+    }
+}
