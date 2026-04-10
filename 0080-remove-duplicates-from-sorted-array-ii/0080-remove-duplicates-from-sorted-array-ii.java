7ec153efca5b72f3ee3a8e2a5e9825c4c14dad14
Nguyễn Minh Châu <91469379+chau-nm@users.noreply.github.com>
2026-04-10 15:03:18 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.java
@@ -1,17 +1,11 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-
-        int slow = 0;
-        for (int fast = slow; fast < nums.length; fast++) {
-            int mapNumber = map.getOrDefault(nums[fast], 0) + 1;
-            map.put(nums[fast], mapNumber);
-
-            if (mapNumber <= 2) {
+        int slow = 2;
+        for (int fast = 2; fast < nums.length; fast++) {
+            if (nums[fast] != nums[slow - 2]) {
                 nums[slow++] = nums[fast];
             }
         }
-
         return slow;
     }
 }
