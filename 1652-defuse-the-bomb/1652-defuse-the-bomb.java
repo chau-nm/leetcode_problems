4b998ab6141be6763bc438e0fa541cde9953e5b9
Nguyễn Minh Châu <91469379+chau-nm@users.noreply.github.com>
2026-04-13 11:06:14 +0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1652-defuse-the-bomb/1652-defuse-the-bomb.java
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.java
@@ -3,20 +3,30 @@ class Solution {
         int n = code.length;
         int[] result = new int[n];
 
+        int previousSum = 0;
         for (int i = 0; i < n; i++) {
             int sum = 0;
 
             if (k > 0) {
-                for (int j = 1; j <= k; j++) {
-                    sum += code[(i + j) % n];
+                if (previousSum > 0) {
+                    sum = previousSum - code[i] + code[(i + k) % n];
+                } else {
+                    for (int j = 1; j <= k; j++) {
+                        sum += code[(i + j) % n];
+                    }
                 }
             }
             if (k < 0) {
-                for (int j = 1; j <= -k; j++) {
-                    sum += code[(i - j + n) % n];
+                if (previousSum > 0) {
+                    sum = previousSum - code[(i - k + n - 1) % n] + code[(i - 1 + n) % n];
+                } else {
+                    for (int j = 1; j <= -k; j++) {
+                        sum += code[(i - j + n) % n];
+                    }
                 }
             }
 
+            previousSum = sum;
             result[i] = sum;
         }
 
