389f2f9c4485b5eb68337921866ffd83f39f3828
Nguyễn Minh Châu <91469379+chau-nm@users.noreply.github.com>
2026-04-28 15:24:25 +0700

    Time: 1 ms (81.04%), Space: 42.3 MB (28.02%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximum69Number (int num) {
+        char[] digits = String.valueOf(num).toCharArray();
+
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i] == '6') {
+                digits[i] = '9';
+                break;
+            }
+        }
+
+        return Integer.parseInt(String.valueOf(digits));
+    }
+}
