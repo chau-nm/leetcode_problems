65ac59e68b105a08c209b23d91bc1757acf82ee0
Nguyễn Minh Châu <91469379+chau-nm@users.noreply.github.com>
2026-06-12 08:59:29 +0700

    Time: 169 ms (26.09%), Space: 292.9 MB (78.26%) - LeetHub

---
+++ 3559-number-of-ways-to-assign-edge-weights-ii/3559-number-of-ways-to-assign-edge-weights-ii.java
@@ -0,0 +1,110 @@
+class Solution {
+
+    private static final long MOD = 1_000_000_007L;
+
+    int[][] up;
+    int[] depth;
+    long[] pow2;
+    int LOG;
+
+    private void dfs(int u, int parent, List<Integer>[] graph) {
+
+        up[u][0] = parent;
+
+        for (int j = 1; j < LOG; j++) {
+            up[u][j] = up[up[u][j - 1]][j - 1];
+        }
+
+        for (int v : graph[u]) {
+            if (v == parent) continue;
+
+            depth[v] = depth[u] + 1;
+            dfs(v, u, graph);
+        }
+    }
+
+    private int lca(int a, int b) {
+
+        if (depth[a] < depth[b]) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+
+        int diff = depth[a] - depth[b];
+
+        for (int j = LOG - 1; j >= 0; j--) {
+            if ((diff & (1 << j)) != 0) {
+                a = up[a][j];
+            }
+        }
+
+        if (a == b) return a;
+
+        for (int j = LOG - 1; j >= 0; j--) {
+            if (up[a][j] != up[b][j]) {
+                a = up[a][j];
+                b = up[b][j];
+            }
+        }
+
+        return up[a][0];
+    }
+
+    public int[] assignEdgeWeights(int[][] edges, int[][] queries) {
+
+        int n = edges.length + 1;
+
+        LOG = 20;
+
+        List<Integer>[] graph = new ArrayList[n + 1];
+
+        for (int i = 0; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int[] e : edges) {
+            int u = e[0];
+            int v = e[1];
+
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        up = new int[n + 1][LOG];
+        depth = new int[n + 1];
+
+        dfs(1, 1, graph);
+
+        pow2 = new long[n + 1];
+        pow2[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            pow2[i] = (pow2[i - 1] * 2) % MOD;
+        }
+
+        int[] ans = new int[queries.length];
+
+        for (int i = 0; i < queries.length; i++) {
+
+            int u = queries[i][0];
+            int v = queries[i][1];
+
+            if (u == v) {
+                ans[i] = 0;
+                continue;
+            }
+
+            int ancestor = lca(u, v);
+
+            int dist =
+                    depth[u]
+                    + depth[v]
+                    - 2 * depth[ancestor];
+
+            ans[i] = (int) pow2[dist - 1];
+        }
+
+        return ans;
+    }
+}
