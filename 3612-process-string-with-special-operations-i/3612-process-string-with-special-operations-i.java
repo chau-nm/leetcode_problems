0dfcf41d8b008c8e9cd3a3f2671a8118862a9e50
Nguyễn Minh Châu <91469379+chau-nm@users.noreply.github.com>
2026-06-16 22:00:56 +0700

    Time: 3 ms (100%), Space: 55.1 MB (64.71%) - LeetHub

---
+++ 3612-process-string-with-special-operations-i/3612-process-string-with-special-operations-i.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder sb = new StringBuilder();
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '*') {
+                if (sb.length() != 0) {
+                    sb.deleteCharAt(sb.length() - 1);
+                }
+            } 
+            else if (ch == '#') {
+                sb.append(sb);
+            } 
+            else if (ch == '%') {
+                sb.reverse();
+            } 
+            else if (ch >= 'a' && ch <= 'z') {
+                sb.append(ch);
+            }
+        }
+
+        return sb.toString();
+    }
+}
